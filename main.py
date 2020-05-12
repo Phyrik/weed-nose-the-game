d7e35763444b3783c08b353175588e4500426a8e
Phyrik <34688504+Phyrik@users.noreply.github.com>
2020-05-12 18:00:15 +0100

    fixed connections issues

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import sys
 import time
 import urllib.request
 
-PORT = 10000
+PORT = 1234
 HEADERSIZE = 20
 TYPEHEADERSIZE = 1
 SENDERHEADERSIZE = 15
@@ -87,7 +87,7 @@ if answer == "h" or answer == "host":
 
     s = multiplayer.Socket(True, socket.gethostname(), PORT)
     players.append({"socket": "host", "address": "host"})
-    print("Send this info to the people you want to join the game: IP - " + urllib.request.urlopen("https://api.ipify.org").read().decode("utf-8") + " Port - " + str(PORT))
+    print("Send this info to the people you want to join the game: IP - " + socket.gethostbyname(socket.gethostname()) + " Port - " + str(PORT))
     s.s.listen(5)
     while len(players) < 4:
         try:
